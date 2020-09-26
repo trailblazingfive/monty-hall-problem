d754eb32d30ebc6c3eba24ae21feb5c9814bbcd4
tbf <contact@trailblazingfive.net>
2020-09-26 18:56:14 +0200

    mock results

---
+++ src/components/Results.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+
+
+const Results = (props) => {
+
+  return (
+    <div>
+      <h1>Simulation results:</h1>
+      <p>66% wins and 34% loses when keeping</p>
+    </div>
+  );
+};
+
+export default Results;
