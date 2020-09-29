5880ae370cee8cd40f3aef55d86614a40b86f7cd
tbf <contact@trailblazingfive.net>
2020-09-29 02:05:23 +0200

    raw results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import styled from 'styled-components';
 
+const StyledResults = styled.p`
+  font-size: 2em;
+  line-height: 1em;
+`
 
 const Results = (props) => {
 
   return (
-    <div>
-      <h1>Simulation results:</h1>
-      <p>66% wins and 34% loses when keeping</p>
-    </div>
+    <StyledResults>
+      <p>Sample size: {props.simulation.sampleSize}</p>
+      <p>Wins {props.simulation.wins}/{props.simulation.sampleSize}</p>
+      <p>Probability of win P={props.simulation.winRate}</p>
+      <p>{(props.simulation.keep) ? 'Sticking to the initial decision' : 'Changing the decision'}</p>
+
+    </StyledResults>
   );
 };
 
