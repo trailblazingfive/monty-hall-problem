ee2b3974598dad3f162c4b3e2f2deb1f2d6285b3
tbf <contact@trailblazingfive.net>
2020-09-29 02:05:14 +0200

    backdrop

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './css/Backdrop.css'
+
+
+const Backdrop = (props) => {
+
+
+  return (
+    <div  className="backdrop space">
+    </div>
+    
+  )
+}
+
+export default Backdrop
---
+++ src/components/LoadingBackdrop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/LoadingBackdrop.css'
+
+
+const LoadingBackdrop = (props) => {
+
+
+  return (
+    <div  className="loadingBackdrop">
+      <div className="loadingTitle">Loading...</div>
+    </div>
+    
+  )
+}
+
+export default LoadingBackdrop
