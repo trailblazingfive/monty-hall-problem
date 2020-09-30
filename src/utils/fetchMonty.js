61ff2dada246cf7aa235b5aa6ae23d133b32c40e
tbf <contact@trailblazingfive.net>
2020-09-30 22:44:12 +0200

    fetch GET to POST

--- src/utils/fetchMonty.js
+++ src/utils/fetchMonty.js
@@ -1,9 +1,31 @@
 const apiUrl = '/api/monty'
 
+// async function fetchMontySimulation(baseUrl, sampleSize, keep, numberOfDoors) {
+//   try {
+//     const requestUrl = baseUrl + `/?sampleSize=${sampleSize}&keep=${keep}&numberOfDoors=${numberOfDoors}`
+//     const response = await fetch(requestUrl)
+//     const data = await response.json()
+//     return data
+//   } catch(error) {
+//     console.error(error)
+//   }
+
+// }
+
 async function fetchMontySimulation(baseUrl, sampleSize, keep, numberOfDoors) {
   try {
-    const requestUrl = baseUrl + `/?sampleSize=${sampleSize}&keep=${keep}&numberOfDoors=${numberOfDoors}`
-    const response = await fetch(requestUrl)
+    const response = await fetch(baseUrl,{
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({
+        sampleSize: sampleSize,
+        keep: keep,
+        numberOfDoors: numberOfDoors
+      })
+    })
     const data = await response.json()
     return data
   } catch(error) {
