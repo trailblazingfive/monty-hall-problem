857991827706689e232b7e82e33e33f6fc193430
tbf <contact@trailblazingfive.net>
2020-09-30 22:44:48 +0200

    conditional output

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -4,16 +4,16 @@ import styled from 'styled-components';
 const StyledOutput = styled.div`
   font-size: 2em;
   line-height: 1em;
+  display: ${props => props.fresh ? 'block' : 'none'};
 `
 
 const Output = (props) => {
-
   return (
-    <StyledOutput>
-      <p>Sample size: {props.simulation.sampleSize}</p>
+    <StyledOutput fresh={props.fresh}>
+      <p>Sample size {props.simulation.sampleSize}</p>
       <p>Wins {props.simulation.wins}/{props.simulation.sampleSize}</p>
       <p>Probability of winning P={props.simulation.winRate}</p>
-      <p>Strategy: {(props.simulation.keep) ? 'sticking to the initial decision' : 'changing the decision'}</p>
+      <p>Strategy {(props.keep) ? 'sticking to the initial decision (KEEP)' : 'changing the decision (CHANGE)'}</p>
     </StyledOutput>
   );
 };
