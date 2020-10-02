1b4fd21a16964d8f63ff8524b6dba7faa720176b
tbf <contact@trailblazingfive.net>
2020-10-02 13:11:20 +0200

    output visibility change

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -4,16 +4,15 @@ import styled from 'styled-components';
 const StyledOutput = styled.div`
   font-size: 2em;
   line-height: 1em;
-  display: ${props => props.fresh ? 'block' : 'none'};
 `
 
 const Output = (props) => {
   return (
-    <StyledOutput fresh={props.fresh}>
-      <p>Sample size {props.simulation.sampleSize}</p>
-      <p>Wins {props.simulation.wins}/{props.simulation.sampleSize}</p>
-      <p>Probability of winning P={props.simulation.winRate}</p>
-      <p>Strategy {(props.keep) ? 'sticking to the initial decision (KEEP)' : 'changing the decision (CHANGE)'}</p>
+    <StyledOutput>
+      <p>Sample size: {props.simulation.sampleSize}</p>
+      <p>Wins: {props.simulation.wins}/{props.simulation.sampleSize}</p>
+      <p>Probability of winning: P={props.simulation.winRate}</p>
+      <p>Strategy: {(props.simulation.keep) ? 'keeping the initial decision (KEEP)' : 'changing the decision (CHANGE)'}</p>
     </StyledOutput>
   );
 };
